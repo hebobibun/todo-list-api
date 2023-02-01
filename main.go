56a49ad7a14fa753bf547ca40b2934536689eb40
libobz <hebobibun@gmail.com>
2023-02-01 19:31:07 +0700

    feat : setup database connection

--- main.go
+++ main.go
@@ -1,5 +1,14 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"todo-api/config"
+)
 
+func main() {
+	cfg := config.InitConfig()
+	db := config.InitDB(*cfg)
+
+	fmt.Println(db)
+	fmt.Println("Database connection is good")
 }
