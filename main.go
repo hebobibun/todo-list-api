1fde3dd305f94e58142f5a2255bb8803d5374938
libobz <hebobibun@gmail.com>
2023-02-01 19:45:17 +0700

    feat : setup intial http server

--- main.go
+++ main.go
@@ -2,13 +2,20 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"todo-api/config"
+
+	"github.com/labstack/echo/v4"
 )
 
 func main() {
+	e := echo.New()
 	cfg := config.InitConfig()
 	db := config.InitDB(*cfg)
 
-	fmt.Println(db)
-	fmt.Println("Database connection is good")
+	fmt.Println(db, "\nconnection is good")
+
+	if err := e.Start(":3030"); err != nil {
+		log.Println(err.Error())
+	}
 }
